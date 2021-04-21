0ad6f79c4eb0317217c9932bfac073e87ef10445
TechStephen <68354435+TechStephen@users.noreply.github.com>
2021-04-21 17:50:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# sorts nums from 0-10000
-nums = [4,8,20,556,0,3400]
-nums.sort()
-print(nums)
+#design
 
-# sorts nums from 10000-0
-nums.sort(reverse = True)
-print(nums)
 
-# sorts names from A-Z
-names = ["Bob","Susan","Mike","John"]
-names.sort(key=str.lower)
-print(names)
 
-# sorts names from Z-A
-names.sort(key=str.lower, reverse = True)
-print(names)
+#implementation
+
+
+
+#functional components
+
+
+#Test cases
