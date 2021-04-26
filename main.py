f6de8006549a58d1d705d24af30f0d028cae909e
TechStephen <68354435+TechStephen@users.noreply.github.com>
2021-04-26 14:22:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-#design
+import tkinter as tk
+from datetime import datetime
 
+#frame
+root = tk.Tk()
+root.geometry("1000x400")
 
+# datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-#implementation
+#widgets
+time = tk.Label(root, text=dt_string)
+time.pack(side=tk.LEFT)
 
+ans = tk.Label(root, text="")
+ans.pack(side=tk.RIGHT)
 
+#conversion
+b = tk.Button(root, text="Convert")
+b.pack(side=tk.CENTER)
 
-#functional components
-
-
-#Test cases
+#tkinter GUI
+root.mainloop()
